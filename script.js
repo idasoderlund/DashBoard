e9390fe331988781c923cee9529fe6509e3ecf40
idasoderlund <soderlundida7711@gmail.com>
2025-04-12 08:58:54 +0200

    minified maincodes in js, minified whole css

--- script.js
+++ script.js
@@ -1,174 +1,130 @@
 function updateClock() {
-  const now = new Date();
-  document.getElementById("clock").innerText = now.toLocaleString();
+  let t = new Date();
+  document.getElementById("clock").innerText = t.toLocaleString();
 }
-setInterval(updateClock, 1000);
-
-document.getElementById("title").addEventListener("blur", function () {
-  localStorage.setItem("dashboardTitle", this.innerText);
-});
-document.getElementById("title").innerText =
-  localStorage.getItem("dashboardTitle") || "My DashBoard;";
-
-//--Links--------------------------------------------------------------------------------------------------------------------------------------------------------------------
-const linkList = document.getElementById("link-List");
-const linkTitleInput = document.getElementById("link-title");
-const linkUrlInput = document.getElementById("link-Url");
-const addLinkButton = document.getElementById("add-link");
-
-addLinkButton.addEventListener("click", function () {
-  const title = linkTitleInput.value;
-  const url = linkUrlInput.value;
-
-  if (title && url) {
-    const li = document.createElement("li");
-    li.innerHTML = `<a href="${url}" target="_blank">${title}</a> <button class="remove-link"> Remove</button>`;
-    linkList.appendChild(li);
-
-    saveLinks();
-    linkTitleInput.value = "";
-    linkUrlInput.value = "";
-  }
-});
-
+setInterval(updateClock, 1e3),
+  document.getElementById("title").addEventListener("blur", function () {
+    localStorage.setItem("dashboardTitle", this.innerText);
+  }),
+  (document.getElementById("title").innerText =
+    localStorage.getItem("dashboardTitle") || "My DashBoard;");
+const linkList = document.getElementById("link-List"),
+  linkTitleInput = document.getElementById("link-title"),
+  linkUrlInput = document.getElementById("link-Url"),
+  addLinkButton = document.getElementById("add-link");
 function saveLinks() {
-  const links = [];
-  document.querySelectorAll("#link-List li").forEach((li) => {
-    const a = li.querySelector("a");
-    if (a) {
-      links.push({ title: a.innerText, url: a.href });
-    }
-  });
-  localStorage.setItem("links", JSON.stringify(links));
+  let t = [];
+  document.querySelectorAll("#link-List li").forEach((e) => {
+    let n = e.querySelector("a");
+    n && t.push({ title: n.innerText, url: n.href });
+  }),
+    localStorage.setItem("links", JSON.stringify(t));
 }
-
 function loadLinks() {
-  const links = JSON.parse(localStorage.getItem("links")) || [];
-  links.forEach((link) => {
-    const li = document.createElement("li");
-    li.innerHTML = `<a href="${link.url}" target="_blank">${link.title}</a> <button class="remove-link">Remove</button>`;
-    linkList.appendChild(li);
+  let t = JSON.parse(localStorage.getItem("links")) || [];
+  t.forEach((t) => {
+    let e = document.createElement("li");
+    (e.innerHTML = `<a href="${t.url}" target="_blank">${t.title}</a> <button class="remove-link">Remove</button>`),
+      linkList.appendChild(e);
   });
 }
-
-linkList.addEventListener("click", function (e) {
-  if (e.target.classList.contains("remove-link")) {
-    e.target.parentElement.remove();
-    saveLinks();
+addLinkButton.addEventListener("click", function () {
+  let t = linkTitleInput.value,
+    e = linkUrlInput.value;
+  if (t && e) {
+    let n = document.createElement("li");
+    (n.innerHTML = `<a href="${e}" target="_blank">${t}</a> <button class="remove-link"> Remove</button>`),
+      linkList.appendChild(n),
+      saveLinks(),
+      (linkTitleInput.value = ""),
+      (linkUrlInput.value = "");
   }
-});
-
-loadLinks();
-
-//toggle-element för add-links----------------------------------------------------------------------------------------------------------------------------
-
-document.addEventListener("DOMContentLoaded", function () {
-  const linkInputsContainer = document.createElement("div");
-  linkInputsContainer.appendChild(linkTitleInput);
-  linkInputsContainer.appendChild(linkUrlInput);
-
-  addLinkButton.addEventListener("click", function () {
-    if (
-      linkTitleInput.style.display === "none" ||
-      linkUrlInput.style.display === "none"
-    ) {
-      linkTitleInput.style.display = "block";
-      linkUrlInput.style.display = "block";
-      addLinkButton.textContent = "Click to Add";
-    } else {
-      linkTitleInput.style.display = "none";
-      linkUrlInput.style.display = "none";
-      addLinkButton.textContent = "+ Add Link";
-    }
+}),
+  linkList.addEventListener("click", function (t) {
+    t.target.classList.contains("remove-link") &&
+      (t.target.parentElement.remove(), saveLinks());
+  }),
+  loadLinks(),
+  document.addEventListener("DOMContentLoaded", function () {
+    let t = document.createElement("div");
+    t.appendChild(linkTitleInput),
+      t.appendChild(linkUrlInput),
+      addLinkButton.addEventListener("click", function () {
+        "none" === linkTitleInput.style.display ||
+        "none" === linkUrlInput.style.display
+          ? ((linkTitleInput.style.display = "block"),
+            (linkUrlInput.style.display = "block"),
+            (addLinkButton.textContent = "Click to Add"))
+          : ((linkTitleInput.style.display = "none"),
+            (linkUrlInput.style.display = "none"),
+            (addLinkButton.textContent = "+ Add Link"));
+      }),
+      (linkTitleInput.style.display = "none"),
+      (linkUrlInput.style.display = "none");
+    let e = document.getElementById("favouriteOptions");
+    e.appendChild(t);
   });
-  linkTitleInput.style.display = "none";
-  linkUrlInput.style.display = "none";
 
-  const favouriteOptions = document.getElementById("favouriteOptions");
-  favouriteOptions.appendChild(linkInputsContainer);
-});
-
-//Väder api----------------------------------------------------------------------------------------------------------------------
+//Fetching geolocation for currentposition----------------------------------------------------------------------------------------------------------------------
 function geoFindMe() {
-  const status = document.querySelector("#status");
-  const mapLink = document.querySelector("#map-link");
-  const weatherInfo = document.getElementById("weather-Info");
-
-  mapLink.href = "";
-  mapLink.textContent = "";
-
-  function success(position) {
-    const latitude = position.coords.latitude;
-    const longitude = position.coords.longitude;
-
-    status.textContent = "";
-    mapLink.href = `https://www.openstreetmap.org/#map=18/${latitude}${longitude}`;
-    mapLink.textContent = `Latitude: ${latitude} °, Longitude: ${longitude} °`;
-
-    localStorage.setItem("latitude", "latitude");
-    localStorage.setItem("longitude", "longitude");
-
-    fetchWeather(latitude, longitude);
-  }
-  function error() {
-    status.textContent = "Unable to retrieve your location";
-  }
-  if (!navigator.geolocation) {
-    status.textContent = "Geolocation is not supported by your browser";
-  } else {
-    status.textContent = "Locating…";
-    navigator.geolocation.getCurrentPosition(success, error);
-  }
+  let t = document.querySelector("#status"),
+    e = document.querySelector("#map-link");
+  document.getElementById("weather-Info"),
+    (e.href = ""),
+    (e.textContent = ""),
+    navigator.geolocation
+      ? ((t.textContent = "Locating…"),
+        navigator.geolocation.getCurrentPosition(
+          function n(o) {
+            let a = o.coords.latitude,
+              r = o.coords.longitude;
+            (t.textContent = ""),
+              (e.href = `https://www.openstreetmap.org/#map=18/${a}${r}`),
+              (e.textContent = `Latitude: ${a} \xb0, Longitude: ${r} \xb0`),
+              localStorage.setItem("latitude", "latitude"),
+              localStorage.setItem("longitude", "longitude"),
+              fetchWeather(a, r);
+          },
+          function e() {
+            t.textContent = "Unable to retrieve your location";
+          }
+        ))
+      : (t.textContent = "Geolocation is not supported by your browser");
 }
-
-async function fetchWeather(lat, lon) {
-  const apiKey = "0759b575b399c7575cc70e35c9ce508c";
+async function fetchWeather(t, e) {
   try {
-    const response = await fetch(
-      `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`
+    let n = await fetch(
+      `https://api.openweathermap.org/data/2.5/forecast?lat=${t}&lon=${e}&appid=0759b575b399c7575cc70e35c9ce508c&units=metric`
     );
-    if (!response.ok) {
-      throw new Error(`Network is out of function ${response.status}`);
-    }
-    const data = await response.json();
-
-    const weatherInfo = document.getElementById("weather-Info");
-    weatherInfo.innerHTML = "";
-
-    const today = new Date();
-    /*const forecastDays = [0, 1, 2];*/
-
+    if (!n.ok) throw Error(`Network is out of function ${n.status}`);
+    let o = await n.json(),
+      a = document.getElementById("weather-Info");
+    a.innerHTML = "";
+    let r = new Date();
     for (let i = 0; i < 3; i++) {
-      const forecastDate = new Date(today);
-      forecastDate.setDate(today.getDate() + i);
-      const forecastDateStr = forecastDate.toISOString().slice(0, 10);
-
-      const dailyForecast = data.list.filter((forecast) =>
-        forecast.dt_txt.startsWith(forecastDateStr)
-      );
-
-      if (dailyForecast.length > 0) {
-        const avgTemp =
-          dailyForecast.reduce((sum, forecast) => sum + forecast.main.temp, 0) /
-          dailyForecast.length;
-        const weatherDescription = dailyForecast[0].weather[0].description;
-        const icon = dailyForecast[0].weather[0].icon;
-
-        const weatherItem = document.createElement("div");
-        weatherItem.className = "weather-item";
-        weatherItem.innerHTML = `
-        <h3>${forecastDate.toLocaleDateString()}</h3>
-        <img src="http://openweathermap.org/img/wn/${icon}@2x.png" alt="${weatherDescription}" alt="${weatherDescription}" />
-        <p>${Math.round(avgTemp)} °C</p>
-        <p>${weatherDescription}</p>
-        `;
-        weatherInfo.appendChild(weatherItem);
+      let l = new Date(r);
+      l.setDate(r.getDate() + i);
+      let c = l.toISOString().slice(0, 10),
+        d = o.list.filter((t) => t.dt_txt.startsWith(c));
+      if (d.length > 0) {
+        let s = d.reduce((t, e) => t + e.main.temp, 0) / d.length,
+          h = d[0].weather[0].description,
+          u = d[0].weather[0].icon,
+          g = document.createElement("div");
+        (g.className = "weather-item"),
+          (g.innerHTML = `
+        <h3>${l.toLocaleDateString()}</h3>
+        <img src="http://openweathermap.org/img/wn/${u}@2x.png" alt="${h}" alt="${h}" />
+        <p>${Math.round(s)} \xb0C</p>
+        <p>${h}</p>
+        `),
+          a.appendChild(g);
       }
     }
-  } catch (error) {
-    console.error("Error trying to fetch data", error);
-    document.getElementById("weather-Info").innerText =
-      "Could not fetch weather data.";
+  } catch (p) {
+    console.error("Error trying to fetch data", p),
+      (document.getElementById("weather-Info").innerText =
+        "Could not fetch weather data.");
   }
 }
 document.addEventListener("DOMContentLoaded", function () {
@@ -176,55 +132,44 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 //-----------------------------------------------------------------------------------------------------------------------------------------
 const noteArea = document.getElementById("note-Area");
-
-noteArea.value = localStorage.getItem("notes") || "";
-
-noteArea.addEventListener("input", function () {
-  localStorage.setItem("notes", noteArea.value);
-});
+(noteArea.value = localStorage.getItem("notes") || ""),
+  noteArea.addEventListener("input", function () {
+    localStorage.setItem("notes", noteArea.value);
+  });
 
 //-----------------------------------------------------------------------------------------------------------------------------------------
 //MainCode to show teatcher for weather api. weather data si fetched from extern api but api key is shown in code.
 document.getElementById("fetchStock").addEventListener("click", function () {
-  const symbol = document.getElementById("symbol").value;
-  const apiKey = "W27I5PQZWEHYBN5R";
-  const url = `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${symbol}&interval=1min&apikey=${apiKey}`;
-
+  let e = document.getElementById("symbol").value;
   fetch(
-    `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${symbol}&interval=1min&apikey=${apiKey}`
+    `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${e}&interval=1min&apikey=W27I5PQZWEHYBN5R`
   )
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error("Network is out of function");
-      }
-      return response.json();
+    .then((e) => {
+      if (!e.ok) throw Error("Network is out of function");
+      return e.json();
     })
-    .then((data) => {
-      if (!data["Time Series (1min)"]) {
-        throw new Error("No data found for the provided symbol");
-      }
-
-      const timeSeries = data["Time Series (1min)"];
-      const latestTime = Object.keys(timeSeries)[0];
-      const latestData = timeSeries[latestTime];
-
-      const stockInfo = `
-    <h2>${symbol.toUpperCase()}</h2>
-    <p>Time: ${latestTime}</p>
-    <p>Opening Price: ${latestData["1. open"]}</p>
-    <p>Most increased Price: ${latestData["2. high"]}</p>
-    <p>Most decreased Price: ${latestData["3. low"]}</p>
-    <p>Closing Price: ${latestData["4. close"]}</p>
-    <p>Volume: ${latestData["5. volume"]}</p>`;
-      document.getElementById("stockInfo").innerHTML = stockInfo;
+    .then((t) => {
+      if (!t["Time Series (1min)"])
+        throw Error("No data found for the provided symbol");
+      let o = t["Time Series (1min)"],
+        n = Object.keys(o)[0],
+        r = o[n],
+        i = `
+    <h2>${e.toUpperCase()}</h2>
+    <p>Time: ${n}</p>
+    <p>Opening Price: ${r["1. open"]}</p>
+    <p>Most increased Price: ${r["2. high"]}</p>
+    <p>Most decreased Price: ${r["3. low"]}</p>
+    <p>Closing Price: ${r["4. close"]}</p>
+    <p>Volume: ${r["5. volume"]}</p>`;
+      document.getElementById("stockInfo").innerHTML = i;
     })
-    .catch((error) => {
-      console.error("Nwtwork error has happened trying to fetch data", error);
-      document.getElementById("stockInfo").innerHTML =
-        "A wrong has occures. Please control the stock-symbol or try again later";
+    .catch((e) => {
+      console.error("Nwtwork error has happened trying to fetch data", e),
+        (document.getElementById("stockInfo").innerHTML =
+          "A wrong has occures. Please control the stock-symbol or try again later");
     });
 });
-
 //-----------------------------------------------------------------------------------------------------------------
 //Keep to show teatcher
 //Code where user writes api key. i don´t know why weather data is not fetched from api even though api key is valid.
